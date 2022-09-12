dc1eb4709ca858ee459ca3f322052ed18bafcfb0
Kirill Kosykh <triplesonicslash@list.ru>
2022-09-12 23:03:38 +0300

    first part

---
+++ src/main/java/com/colossus/spsecurity/appuser/AppUserService.java
@@ -0,0 +1,21 @@
+package com.colossus.spsecurity.appuser;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AppUserService implements UserDetailsService {
+
+    private final static String USER_NOT_FOUND_MSG = "user with email %s not found";
+    private final AppUserRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repo.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG,email)));
+    }
+}
