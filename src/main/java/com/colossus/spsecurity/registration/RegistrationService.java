6739dfe61329c7e5cb3dac197711ae7e6a335768
Kirill Kosykh <triplesonicslash@list.ru>
2022-09-13 23:02:29 +0300

    registration service and email validator

--- src/main/java/com/colossus/spsecurity/registration/RegistrationService.java
+++ src/main/java/com/colossus/spsecurity/registration/RegistrationService.java
@@ -1,10 +1,28 @@
 package com.colossus.spsecurity.registration;
 
+import com.colossus.spsecurity.appuser.AppUser;
+import com.colossus.spsecurity.appuser.AppUserRole;
+import com.colossus.spsecurity.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
+    @PostMapping
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) throw new IllegalStateException(String.format("%s isn't valid", request.getEmail()));
+        return appUserService.signUpUser(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER));
     }
 }
